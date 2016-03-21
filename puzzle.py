8e38bf071f551769775edfe4d53b48457c4a418c
Mary Connolly <mconnol6@nd.edu>
2016-03-21 00:41:30 -0400

    changed puzzle so that it can be used for project 2

--- puzzle.py
+++ puzzle.py
@@ -54,66 +54,66 @@ def rotate_tile(tileList, numberOfTiles):
 class puzzle:
 
 	def __init__(self):
-		x = 5
 
-	def play(self):
-
-		numberOfTiles = random.randint(4,10)
-		puzzleSize = numberOfTiles
+		#self.tileList is list of possible tiles, self.solutionList is list of actual tiles that solve puzzle
+		#puzzleList is list of tiles that user has added to solve the puzzle
+		self.tileList = []
+		self.solutionList = []
+		self.puzzleList = []
 
 		#height and width
-		h = 400
-		w = get_window_width(puzzleSize,numberOfTiles)
+		self.h = 400
+		self.w = 600
+		self.tile_size = 75
 
-		tile_size = 75
-
+	def create_random_puzzle(self):
+		
 		#x and y coordinates of upper left of first tile
 		space = 50
-		x = get_print_start_point(w,space,numberOfTiles,tile_size)
+		x = get_print_start_point(self.w,space,self.numberOfTiles,self.tile_size)
 		y = 250
-		s = tile_size + space#space b/t tiles
-		tileList = []
-		solutionList = []
-		blankTile = Tile(600,600,tile_size,(WHITE,WHITE,WHITE,WHITE))
-
-		"""tileList.append(Tile(x, y, tile_size, colorList[0]))
-		tileList.append(Tile(x+s, y, tile_size, colorList[1]))
-		tileList.append(Tile(x+2*s, y, tile_size, colorList[2]))
-		tileList.append(Tile(x+3*s, y, tile_size, colorList[3]))
-		solutionList.append(blankTile)
-		solutionList.append(blankTile)
-		solutionList.append(blankTile)
-		solutionList.append(blankTile)"""
+		s = self.tile_size + space#space b/t tiles
+		blankTile = Tile(600,600,self.tile_size,(WHITE,WHITE,WHITE,WHITE))
 
 		#make tiles based on previous tile color
-		tileList.append(Tile(x, y, tile_size))
-		solutionList.append(blankTile)
-		for i in range(1,numberOfTiles):
-			tileList.append(Tile(x+i*s, y,tile_size,tileList[i-1].get_right_color()))
-			solutionList.append(blankTile)
+		self.tileList.append(Tile(x, y, self.tile_size))
+		self.solutionList.append(blankTile)
+		for i in range(1,self.numberOfTiles):
+			self.tileList.append(Tile(x+i*s, y,self.tile_size,self.tileList[i-1].get_right_color()))
+			self.solutionList.append(blankTile)
 
 		#puzzle draw
-		xP = get_print_start_point(w,0,puzzleSize,tile_size)
+		xP = get_print_start_point(self.w,0,self.puzzleSize,self.tile_size)
 		yP = 75
 		sP = 75 #space between pieces
 		shortEdge = 12.5
-		puzzleList = []
 
 		#generate puzzle based on tile colors
-		puzzleList.append(get_front_of_puzzle(tileList[0],xP,yP,tile_size,shortEdge))
-		for k in range(1,puzzleSize-1):
-			puzzleList.append(get_middle_of_puzzle(tileList[k],xP+k*sP,yP,tile_size,shortEdge))
-		puzzleList.append(get_end_of_puzzle(tileList[puzzleSize-1],xP+(puzzleSize-1)*sP,yP,tile_size,shortEdge))
+		self.puzzleList.append(get_front_of_puzzle(self.tileList[0],xP,yP,self.tile_size,shortEdge))
+		for k in range(1,self.puzzleSize-1):
+			self.puzzleList.append(get_middle_of_puzzle(self.tileList[k],xP+k*sP,yP,self.tile_size,shortEdge))
+		self.puzzleList.append(get_end_of_puzzle(self.tileList[self.puzzleSize-1],xP+(self.puzzleSize-1)*sP,yP,self.tile_size,shortEdge))
 
+		#random shuffle of self.tileList
+		self.tileList = shuffle_tile(self.tileList,self.numberOfTiles)
 
-		#random shuffle of tileList
-		tileList = shuffle_tile(tileList,numberOfTiles)
+	def play(self):
+
+		self.numberOfTiles = random.randint(4,10)
+		self.puzzleSize = self.numberOfTiles
+
+		self.w = get_window_width(self.puzzleSize,self.numberOfTiles)
+
+		#set up puzzle and tiles for user's selection
+		self.create_random_puzzle()
 
 		#current selected tile
 		currentTile = 0
+		
+		blankTile = Tile(600,600,self.tile_size,(WHITE,WHITE,WHITE,WHITE))
 
 		#pygame.init()
-		screen = pygame.display.set_mode((w, h))
+		screen = pygame.display.set_mode((self.w, self.h))
 		pygame.display.set_caption("Course Project 1: Interactive Puzzle")
 		pygame.display.set_icon(pygame.image.load('objects/tile.bmp'))
 
@@ -132,11 +132,11 @@ class puzzle:
 
 			#print to screen
 			screen.fill(WHITE)
-			for piece in puzzleList:
+			for piece in self.puzzleList:
 				piece.draw(screen)
-			for tile in tileList:
+			for tile in self.tileList:
 				tile.draw(screen)
-			for solution in solutionList:
+			for solution in self.solutionList:
 				solution.draw(screen)
 
 			#check if user has pressed a key
@@ -152,18 +152,18 @@ class puzzle:
 
 					#iterate through pieces in puzzleList to see if mouse is inside
 					currentPiece = 0
-					for piece in puzzleList:
+					for piece in self.puzzleList:
 						currentPiece += 1
 						if piece.is_inside(pos):
-							solutionList[currentPiece-1] = blankTile
+							self.solutionList[currentPiece-1] = blankTile
 
 			if event.type == pygame.MOUSEBUTTONUP:
 				pos = pygame.mouse.get_pos()
 
-				#get number of tile from tileList (lower tiles) if one was clicked
+				#get number of tile from self.tileList (lower tiles) if one was clicked
 				#currentTile is number of selected tile from choices
 				place = 0
-				for tile in tileList:
+				for tile in self.tileList:
 					place += 1
 					if tile.is_inside(pos):
 						if currentTile == 0:
@@ -172,36 +172,27 @@ class puzzle:
 				#iterate through each piece in puzzleList (upper tiles)
 				#check to see if mouse click was inside each piece
 				currentPiece = 0
-				for piece in puzzleList:
+				for piece in self.puzzleList:
 					currentPiece += 1
 					if piece.is_inside(pos):
 						#if user has selected a tile to place
 						if currentTile != 0:
 							rect = piece.get_rect() #rect for piece in puzzleList
-							tile = Tile(rect[0],rect[1],rect[2],tileList[currentTile-1].get_color()) #create tile to place in puzzle
-							solutionList[currentPiece-1] = tile #place in puzzle
+							tile = Tile(rect[0],rect[1],rect[2],self.tileList[currentTile-1].get_color()) #create tile to place in puzzle
+							self.solutionList[currentPiece-1] = tile #place in puzzle
 
 						#if user has not selected a tile to place
 						else:
 							#if selected tile in puzzle is not blank
-							if solutionList[currentPiece-1] != blankTile:
+							if self.solutionList[currentPiece-1] != blankTile:
 								rect = piece.get_rect() #rect for piece in puzzleList
-								c = solutionList[currentPiece-1].get_color() #get color list for tiles
+								c = self.solutionList[currentPiece-1].get_color() #get color list for tiles
 								newColors = ((c[3], c[0], c[1], c[2]))
 								tile = Tile(rect[0], rect[1], rect[2], newColors) #create new tile to replace the old one
-								solutionList[currentPiece-1] = tile #place in puzzle
+								self.solutionList[currentPiece-1] = tile #place in puzzle
 
 						currentTile = 0
 							
-					"""# If puzzle is filled, check solution
-					for i in range(4,8):
-						if tileList[i] == blankTile:
-							solved = False
-							break
-						elif i == 7:
-							solved = checkSolution(puzzleList, tileList)
-					"""
-
 					# Display solution status
 					font = pygame.font.Font(None, 30)
 					text = ''
@@ -209,7 +200,6 @@ class puzzle:
 						text = font.render("Valid Solution", True, GREEN)
 					else: 
 						text = font.render("Valid Solution", True, WHITE)
-					screen.blit(text, (w/2-75, h/2))
+					screen.blit(text, (self.w/2-75, self.h/2))
 
 			pygame.display.flip()
-		#pygame.display.quit()
