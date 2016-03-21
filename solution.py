6a1e509cf4edda9e82fc5e30da3d2521d5f61988
John Rocha <john.rocha5@gmail.com>
2016-03-20 21:32:54 -0400

    Added function that adds children to a node if they exist

--- solution.py
+++ solution.py
@@ -10,6 +10,7 @@ class Node(object):
         self.tile = None
         self.children = []
         self.parent = None
+        self.exhausted = False
 
     # Add a tile to current node's children pointing to it as parent 
     def add_child(self, tile):
@@ -35,3 +36,14 @@ class SolutionDFA(object):
             possible.append((color[2], color[3], color[0], color[1]))
             possible.append((color[3], color[0], color[1], color[2]))
         return possible
+
+    # Populate a node's children data given 3 or 4 edge colors
+    def find_possible_children(self, edge_colors):
+        children = [] 
+        for possible in self.possible_tiles:
+            check = possible
+            if len(edge_colors) == 3:
+                check.pop(1)            # Don't care about the rightmost color
+            if check == edge_colors:
+                children.append(possible)
+        return children
